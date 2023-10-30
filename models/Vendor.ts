d6c7b9b206d12652fde3f3dc4420501a5850f0b6
=oaksas <=>
2023-10-30 15:54:12 +0300

    Update return value of vendor

--- models/Vendor.ts
+++ models/Vendor.ts
@@ -32,6 +32,15 @@ const VendorSchema = new Schema({
     rating: { type: Number },
     foods: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'food' }],
 }, {
+    toJSON: {
+        transform(doc, ret) {
+            delete ret.password;
+            delete ret.salt;
+            delete ret.__v;
+            delete ret.createdAt;
+            delete ret.updatedAt;
+        }
+    },
     timestamps: true
 })
 
