555dd7b1cd47110c3651a31de5650397746cecd0
=oaksas <=>
2023-10-31 15:13:46 +0300

    Add generate signature && verify signature helper functions

--- utils/GenerateHashedPassword.ts
+++ utils/GenerateHashedPassword.ts
@@ -1,4 +1,8 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { VendorPayload } from '../dto';
+import { JWT_SECRET } from '../config';
+
 
 export const GenerateHash = async () => {
     return await bcrypt.genSalt()
@@ -7,4 +11,17 @@ export const GenerateHash = async () => {
 export const GenerateHashedPassword = async (password: string, salt: string) => {
 
     return await bcrypt.hash(password, salt)
+}
+
+export const ValidatePassword = async (password: string, salt: string, savedPassword: string) => {
+    return await GenerateHashedPassword(password, salt) === savedPassword
+}
+
+export const GenerateSignature = (payload: VendorPayload) => {
+    return jwt.sign(payload, JWT_SECRET, { expiresIn: '30m' })
+
+}
+
+export const ValidateSignature = (token: string) => {
+    return jwt.verify(token, JWT_SECRET)
 }
