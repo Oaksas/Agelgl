045693b714f214332ed8210272a1f5b67786f079
=oaksas <=>
2023-10-31 11:23:08 +0300

    Add vendor route

--- routes/VendorRoute.ts
+++ routes/VendorRoute.ts
@@ -2,4 +2,18 @@ import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
+
+router.post('/login', (req: Request, res: Response) => {
+
+    res.send("Login")
+}
+)
+
+
+router.get('/', (req: Request, res: Response) => {
+    res.send("Vendor route")
+}
+)
+
+
 export { router as VendorRoute }
