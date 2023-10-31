bc7d3c4ba4ede74683a66f35e3a5791f6d1cfb63
=oaksas <=>
2023-10-31 11:22:57 +0300

    Fix

--- routes/AdminRoute.ts
+++ routes/AdminRoute.ts
@@ -1,10 +1,10 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import { CreateVendor, GetVendorById, GetVendors } from '../controllers';
 
 const router = express.Router();
 
 router.get('/vendors', GetVendors)
-router.get('vendor/:id', GetVendorById)
+router.get('/vendor/:id', GetVendorById)
 router.post('/vendor', CreateVendor);
 
 
