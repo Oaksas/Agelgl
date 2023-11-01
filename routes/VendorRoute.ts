3e7c514068da310fc8bc27aa894d6fc959ece83e
=oaksas <=>
2023-11-01 11:00:05 +0300

    Add routes

--- routes/VendorRoute.ts
+++ routes/VendorRoute.ts
@@ -1,13 +1,16 @@
 import express, { Request, Response } from 'express';
 import { VendorLogin, VendorProfile, VendorUpdateProfile, VendorUpdateService } from '../controllers';
+import { Authenticate } from '../middlewares/CommonAuth';
 
 const router = express.Router();
 
 
 router.post('/vendor/login', VendorLogin)
-router.get('/vendor/profile', VendorProfile)
-router.patch('/vendor/profile', VendorUpdateProfile)
 
+router.use(Authenticate)
+
+router.get('/profile/vendor', VendorProfile)
+router.patch('/vendor/profile', VendorUpdateProfile)
 router.patch('/vendor/service', VendorUpdateService)
 
 
