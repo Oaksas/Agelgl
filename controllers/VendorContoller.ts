88a6cee22318114e5d2b0c63a963d94962bf35cb
=oaksas <=>
2023-11-01 10:58:33 +0300

    Add update profile && service controllers

--- controllers/VendorContoller.ts
+++ controllers/VendorContoller.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
-import { VendroLoginInputs } from "../dto";
+import { EditVendorProfile, VendroLoginInputs } from "../dto";
 import { FindVendor } from "./AdminController";
-import { GenerateSignature, ValidatePassword } from "../utils/GenerateHashedPassword";
+import { GenerateSignature, ValidatePassword } from "../utils";
 
 
 export const VendorLogin = async (req: Request, res: Response, next: NextFunction) => {
@@ -32,13 +32,91 @@ export const VendorLogin = async (req: Request, res: Response, next: NextFunctio
 }
 
 export const VendorProfile = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const user = req.user;
+        if (user) {
+            const userProfile = await FindVendor(user._id);
+            return res.status(200).json({
+                "data": userProfile
+            })
+        }
+        else {
+            return res.status(401).json({
+                "message": "Unauthorized"
+            })
+        }
+    } catch (error) {
+
+        console.log(error)
+    }
 
 }
 
 export const VendorUpdateProfile = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { name, phone, foodTypes, address } = <EditVendorProfile>req.body;
+
+        const user = req.user;
+        if (user) {
+            const userProfile = await FindVendor(user._id);
+
+            if (userProfile) {
+                userProfile.name = name;
+                userProfile.phone = phone;
+                userProfile.foodType = foodTypes;
+                userProfile.address = address;
+                const result = await userProfile.save();
+                return res.status(200).json({
+                    "data": result
+                })
+            }
+            return res.status(200).json({
+                "data": userProfile
+            })
+        }
+        else {
+            return res.status(401).json({
+                "message": "Unauthorized"
+            })
+        }
+    } catch (error) {
+
+        return res.status(500).json({
+            "message": "Something went wrong"
+        })
+    }
+
 
 }
 
 export const VendorUpdateService = async (req: Request, res: Response, next: NextFunction) => {
 
+
+    try {
+        const user = req.user;
+        if (user) {
+            const userProfile = await FindVendor(user._id);
+
+            if (userProfile) {
+                userProfile.serviceAvailable = !userProfile.serviceAvailable;
+                const result = await userProfile.save();
+                return res.status(200).json({
+                    "data": result
+                })
+            }
+            return res.status(200).json({
+                "data": userProfile
+            })
+        }
+        else {
+            return res.status(401).json({
+                "message": "Unauthorized"
+            })
+        }
+    } catch (error) {
+
+        return res.status(500).json({
+            "message": "Something went wrong"
+        })
+    }
 }
