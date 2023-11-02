f3a919660460e59384ae4b33666382de9b7a7c8c
=oaksas <=>
2023-11-02 14:13:30 +0300

    Refactor app into services

--- index.ts
+++ index.ts
@@ -1,27 +1,15 @@
-import bodyParser from 'body-parser';
 import express from 'express';
-import { AdminRoute, VendorRoute } from './routes';
-import mongoose from 'mongoose';
-import { MONGO_URI } from './config';
-
-const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(AdminRoute);
-app.use(VendorRoute);
+import App from './services/ExpressApp';
+import db from './services/Database';
 
 
-mongoose.connect('mongodb://0.0.0.0:27017/fooddelivery', {
-}).then(() => {
-    console.log("Connected to Database !!!")
-}).catch((err) => {
-    console.log(err)
-})
+const StartServer = async () => {
+    const app = express();
+    await db();
+    await App(app);
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000")
+    })
+}
 
-
-app.listen(3000, () => {
-    console.clear()
-    console.log('Server is running on port 3000')
-})
+StartServer();
