0256c6550a13fc60d215a0e3f43081f81166c8e7
=oaksas <=>
2023-11-02 14:14:10 +0300

    Add vendor routes

--- routes/VendorRoute.ts
+++ routes/VendorRoute.ts
@@ -1,18 +1,35 @@
 import express, { Request, Response } from 'express';
-import { VendorLogin, VendorProfile, VendorUpdateProfile, VendorUpdateService } from '../controllers';
+import { AddFood, GetFoodById, GetFoods, VendorLogin, VendorProfile, VendorUpdateProfile, VendorUpdateProfilePicture, VendorUpdateService } from '../controllers';
 import { Authenticate } from '../middlewares/CommonAuth';
+import multer from 'multer';
 
 const router = express.Router();
 
+const imageStorage = multer.diskStorage({
 
-router.post('/vendor/login', VendorLogin)
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+const images = multer({ storage: imageStorage }).array('images', 10);
+
+
+router.post('/login', VendorLogin)
 
 router.use(Authenticate)
 
-router.get('/profile/vendor', VendorProfile)
-router.patch('/vendor/profile', VendorUpdateProfile)
-router.patch('/vendor/service', VendorUpdateService)
+router.get('/profile', VendorProfile)
+router.patch('/profile', VendorUpdateProfile)
+router.patch('/coverimage', images, VendorUpdateProfilePicture)
+router.patch('/service', VendorUpdateService)
 
+router.post('/food', images, AddFood)
+router.get('/food/:id', GetFoodById)
+router.get('/foods', GetFoods)
 
 
 router.get('/')
