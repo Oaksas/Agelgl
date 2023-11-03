cc41b58d0d7c4d2c26c83d5e0e5450e36e43452f
=oaksas <=>
2023-11-03 14:19:33 +0300

    Refactor

--- services/ExpressApp.ts
+++ services/ExpressApp.ts
@@ -1,6 +1,6 @@
 import bodyParser from 'body-parser';
 import express, { Application } from 'express';
-import { AdminRoute, VendorRoute } from '../routes';
+import { AdminRoute, CustomerRoute, VendorRoute } from '../routes';
 import path from 'path';
 import { ShoppingRoute } from '../routes/ShoppingRoute';
 
@@ -10,9 +10,10 @@ export default async (app: Application) => {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use('./images', express.static(path.join(__dirname, 'images')));
 
-    app.use(AdminRoute);
+    app.use('/admin', AdminRoute);
     app.use('/vandor', VendorRoute);
-    app.use('/shop', ShoppingRoute)
+    app.use('/shop', ShoppingRoute);
+    app.use('/user', CustomerRoute)
 
     return app;
 }
